63c725d8ac780faf759663c9a8ffabed6f0b0bec
Joshua Gilbert <gilbertman77@yahoo.com>
2021-07-29 20:27:33 +0000

    Add bounce

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 	# Simple Pygame Program
 
+  ## HOMEWORK
+  # 0) Get this file from Github
+  # 1) Fix X bounce
+  # 2) Condense if statements for bouncing
+  # 3) Make bounce slight random angle
+  # 4) Setup Pi and run the SH for adaptive cruise
+
 	# Import and initialize the pygame library
 import pygame
 pygame.init()
 
+scnHeight = 500
+scnWidth = 500
 	# Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode([scnWidth, scnHeight])
+
+
+
+xPos = 100
+yPos = 200
+YELLOW = (255, 255, 0)
+position = [100, 200]
 
 	# Run until user asks to quit
 running = True
+xVel = .5
+yVel = xVel
 
-height = .1 
 	# -------- Main Program Loop -----------
 while running:
   # --- Main event loop
@@ -18,12 +35,21 @@ while running:
     if event.type == pygame.QUIT:
       running = False
 		
-  # Fill the backgroung with white
-  screen.fill((255, 0, 0))
+  # Fill the backgroung with yellow
+  screen.fill(YELLOW)
 
   # Draw a solid blue circle in the center    
-  pygame.draw.circle(screen, (0, 0, 255), (xPos, yPos), 10)
+  #pygame.draw.circle(screen, (0, 0, 255), (xPos, yPos), 10)
+  pygame.draw.circle(screen, (0, 0, 255), position, 10)
 
+  if position[1] >= scnHeight:
+    yVel = yVel * -1
+  elif position[1] <= 0:
+    yVel = yVel * -1
+
+  position[1] = position[1] + yVel
+  #print(position[1], position[0])
+  
   # Flip the display
   pygame.display.flip()
 	 
